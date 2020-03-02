512264d30c0e9d8edf604ddc3192e9d535beb8bc
ambiyo-marion <ambiyomarion@gmail.com>
2020-03-02 05:59:36 +0300

    added the submit function

--- js/script.js
+++ js/script.js
@@ -9,3 +9,11 @@ $(document).ready(function(){
           alert(" Your pizza will be delivered at " + getLocation + " with additional amount of 1$ for delivery");
       }
   })
+  $("#submit").click(function(event){
+    var Size = parseFloat(document.getElementById('size').value);
+    var Crust = parseFloat(document.getElementById('crust').value);
+    var Topping = parseFloat(document.getElementById('toppings').value);
+    var Quantity = parseFloat(document.getElementById('num').value);
+    var total = (Size + Crust + Topping + Quantity + Amount);
+  
+    
